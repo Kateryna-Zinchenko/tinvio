e65a1d5cfce631b32b4407d3b499696453d51ddf
Kirill Usichenko <kirillusichenko@gmail.com>
2022-08-16 19:42:41 +0300

    fix: adaptive for done component, company page

--- src/components/pages/Company/components/done/Done.tsx
+++ src/components/pages/Company/components/done/Done.tsx
@@ -1,67 +1,193 @@
 import React from 'react';
 import {
+    DotWrapper,
     H1,
-    ImgDevice, ImgScreen, Tabs,
-    TabWrapper2,
-    TabWrapper4, Title,
-    Wrapper, Wrapper1
+    ImgDevice, ImgScreen, Li, Li1, LightDot, LightDot3, List, List1, LiWrapper, LiWrapper1, Tabs, TabWrapper1,
+    TabWrapper2, TabWrapper3,
+    TabWrapper4, TabWrapper5, Title,
+    Wrapper, Wrapper1, Year3, Year1, Year5, Li5
 } from "./DoneStyles";
-import DoneTab from "../../../../common/doneTab/DoneTab";
 
 const Done = () => {
+
     return (
         <section className='done'>
             <Wrapper>
                 <H1>What we’ve done</H1>
                 <Tabs>
-                    <DoneTab
-                        year={'2021'}
-                        Li1={'Launch Tinvio Checkout for on-platform payments, supplier web dashboard and more'}
-                        Li2={'Series A funding to build embedded supply chain financing products for our users'}
-                        Li3={'Expand into new verticals and markets. Close the year with 200 team members and 4000+ active '
-                            + 'businesses across the region'}
-                        margin={'0'}
-                        borderRadius={'0 32px 32px 0'}
-                        width1024={'500px'}
-                        yearFontSize768
-                        yearLineHeight768
-                        yearFontSize1920
-                        yearLineHeight1920
-                        liFontSize768
-                        liLineHeight768
-                        liFontSize1920
-                        liLineHeight1920
-                        liWrapMargin768
-                        listPadding768
-                    />
+                    {/*<DoneTab*/}
+                    {/*    year={'2021'}*/}
+                    {/*    Li1={'Launch Tinvio Checkout for on-platform payments, supplier web dashboard and more'}*/}
+                    {/*    Li2={'Series A funding to build embedded supply chain financing products for our users'}*/}
+                    {/*    Li3={'Expand into new verticals and markets. Close the year with 200 team members and 4000+ active '*/}
+                    {/*        + 'businesses across the region'}*/}
+                    {/*    margin={'0'}*/}
+                    {/*    borderRadius={'0 32px 32px 0'}*/}
+                    {/*    width1024={'500px'}*/}
+                    {/*    yearFontSize768*/}
+                    {/*    yearLineHeight768*/}
+                    {/*    yearFontSize1920*/}
+                    {/*    yearLineHeight1920*/}
+                    {/*    liFontSize768*/}
+                    {/*    liLineHeight768*/}
+                    {/*    liFontSize1920*/}
+                    {/*    liLineHeight1920*/}
+                    {/*    liWrapMargin768*/}
+                    {/*    listPadding768*/}
+                    {/*/>*/}
+                    {/*<Wrapper1>*/}
+                    {/*    <TabWrapper2>*/}
+                    {/*        <ImgScreen src='/assets/images/company/done-screen.png'/>*/}
+                    {/*    </TabWrapper2>*/}
+                    {/*    <DoneTab*/}
+                    {/*        year={'2020'}*/}
+                    {/*        Li1={'Seed funding led by Sequoia, GFC'}*/}
+                    {/*        Li2={'Launch multi-country localization, digital invoicing and more'}*/}
+                    {/*        Li3={'Adapt to Covid-19. Triple our team size to 100. Activate 1000+ new businesses'}*/}
+                    {/*        dotColor*/}
+                    {/*        background*/}
+                    {/*        textColor={'#FFFFFF'}*/}
+                    {/*        borderRadius={'32px'}*/}
+                    {/*    />*/}
+                    {/*</Wrapper1>*/}
+
+                    {/*<DoneTab*/}
+                    {/*    year={'Q3 2019'}*/}
+                    {/*    Li1={'Our founder Ajay raises pre-seed in Europe. Moves back home to Singapore to start Tinvio'}*/}
+                    {/*    Li2={'Zero to one in team, product, processes. Launch merchant mobile app'}*/}
+                    {/*    Li3={'Ajay meets our COO Tedo. Expand into Indonesia and more markets'}*/}
+                    {/*    borderRadius={'32px 0 0 32px'}*/}
+                    {/*    width768={'376px'}*/}
+                    {/*    margin768={'16px 0 0'}*/}
+                    {/*/>*/}
+
+                    <TabWrapper1>
+                        <Year1>
+                            2021
+                        </Year1>
+                        <List1>
+                            <LiWrapper1>
+                                <div>
+                                    <DotWrapper>
+                                        <LightDot/>
+                                    </DotWrapper>
+                                </div>
+                                <Li1>
+                                    Launch Tinvio Checkout for on-platform payments, supplier web dashboard and more
+                                </Li1>
+                            </LiWrapper1>
+                            <LiWrapper1>
+                                <div>
+                                    <DotWrapper>
+                                        <LightDot/>
+                                    </DotWrapper>
+                                </div>
+                                <Li1>
+                                    Series A funding to build embedded supply chain financing products for our users
+                                </Li1>
+                            </LiWrapper1>
+                            <LiWrapper1>
+                                <div>
+                                    <DotWrapper>
+                                        <LightDot/>
+                                    </DotWrapper>
+                                </div>
+                                <Li1>
+                                    Expand into new verticals and markets. Close the year with 200 team members and
+                                    4000+ active businesses across the region
+                                </Li1>
+                            </LiWrapper1>
+                        </List1>
+                    </TabWrapper1>
+
                     <Wrapper1>
                         <TabWrapper2>
                             <ImgScreen src='/assets/images/company/done-screen.png'/>
                         </TabWrapper2>
-                        <DoneTab
-                            year={'2020'}
-                            Li1={'Seed funding led by Sequoia, GFC'}
-                            Li2={'Launch multi-country localization, digital invoicing and more'}
-                            Li3={'Adapt to Covid-19. Triple our team size to 100. Activate 1000+ new businesses'}
-                            dotColor
-                            background
-                            textColor={'#FFFFFF'}
-                            borderRadius={'32px'}
-                        />
+
+                        <TabWrapper3>
+                            <Year3>
+                                2020
+                            </Year3>
+                            <List>
+                                <LiWrapper>
+                                    <div>
+                                        <DotWrapper>
+                                            <LightDot3/>
+                                        </DotWrapper>
+                                    </div>
+                                    <Li>
+                                        Seed funding led by Sequoia, GFC
+                                    </Li>
+                                </LiWrapper>
+                                <LiWrapper>
+                                    <div>
+                                        <DotWrapper>
+                                            <LightDot3/>
+                                        </DotWrapper>
+                                    </div>
+                                    <Li>
+                                        Launch multi-country localization, digital invoicing and more
+                                    </Li>
+                                </LiWrapper>
+                                <LiWrapper>
+                                    <div>
+                                        <DotWrapper>
+                                            <LightDot3/>
+                                        </DotWrapper>
+                                    </div>
+                                    <Li>
+                                        Adapt to Covid-19. Triple our team size to 100. Activate 1000+ new businesses
+                                    </Li>
+                                </LiWrapper>
+                            </List>
+                        </TabWrapper3>
                     </Wrapper1>
+
                     <TabWrapper4>
                         <Title>Dark mode too!</Title>
                         <ImgDevice src='/assets/images/company/done-device.png'/>
                     </TabWrapper4>
-                    <DoneTab
-                        year={'Q3 2019'}
-                        Li1={'Our founder Ajay raises pre-seed in Europe. Moves back home to Singapore to start Tinvio'}
-                        Li2={'Zero to one in team, product, processes. Launch merchant mobile app'}
-                        Li3={'Ajay meets our COO Tedo. Expand into Indonesia and more markets'}
-                        borderRadius={'32px 0 0 32px'}
-                        width768={'376px'}
-                        margin768={'16px 0 0'}
-                    />
+
+                    <TabWrapper5>
+                        <Year5>
+                            Q3 2019
+                        </Year5>
+                        <List>
+                            <LiWrapper>
+                                <div>
+                                    <DotWrapper>
+                                        <LightDot/>
+                                    </DotWrapper>
+                                </div>
+                                <Li5>
+                                    Our founder Ajay raises pre-seed in Europe. Moves back home to Singapore
+                                    to start Tinvio
+                                </Li5>
+                            </LiWrapper>
+                            <LiWrapper>
+                                <div>
+                                    <DotWrapper>
+                                        <LightDot/>
+                                    </DotWrapper>
+                                </div>
+                                <Li5>
+                                    Zero to one in team, product, processes. Launch merchant mobile app
+                                </Li5>
+                            </LiWrapper>
+                            <LiWrapper>
+                                <div>
+                                    <DotWrapper>
+                                        <LightDot/>
+                                    </DotWrapper>
+                                </div>
+                                <Li5>
+                                    Ajay meets our COO Tedo. Expand into Indonesia and more markets
+                                </Li5>
+                            </LiWrapper>
+                        </List>
+                    </TabWrapper5>
+
                 </Tabs>
             </Wrapper>
         </section>
--- src/components/pages/Company/components/done/DoneStyles.tsx
+++ src/components/pages/Company/components/done/DoneStyles.tsx
@@ -42,10 +42,11 @@ export const Wrapper1 = styled.div`
   @media (min-width: 768px) {
     display: flex;
     flex-direction: row-reverse;
+    margin: 16px 0 0;
   }
   @media (min-width: 1024px) {
-    flex-direction: row;
-    flex-wrap: wrap;
+    display: unset;
+    margin: 0;
   }
 `;
 
@@ -58,13 +59,25 @@ export const TabWrapper2 = styled.div`
 
   @media (min-width: 768px) {
     min-width: 360px;
-    margin: 16px 16px 0;
+    margin: 0 16px;
     padding: 16px 0 16px 26px;
   }
   @media (min-width: 1024px) {
     min-width: 500px;
     margin: 0;
     padding: 16px 22px;
+  }
+  @media (min-width: 1280px) {
+    width: 574px;
+    height: 403px;
+  }
+  @media (min-width: 1440px) {
+    width: 654px;
+    height: 380px;
+  }
+  @media (min-width: 1920px) {
+    width: 714px;
+    height: 391px;
   }
 `;
 
@@ -82,6 +95,17 @@ export const TabWrapper4 = styled.div`
   @media (min-width: 768px) {
     width: 376px;
     height: 311px;
+  }
+  @media (min-width: 1024px) {
+    width: 340px;
+    height: 342px;
+  }
+  @media (min-width: 1280px) {
+    width: 404px;
+    height: 340px;
+  }
+  @media (min-width: 1440px) {
+    height: 298px;
   }
 `;
 
@@ -114,6 +138,260 @@ export const ImgScreen = styled.img`
     margin: 0;
     float: unset;
   }
+  @media (min-width: 1280px) {
+    max-width: 530px;
+  }
+  @media (min-width: 1440px) {
+    max-width: 497px;
+    margin: 0 auto;
+  }
+  @media (min-width: 1920px) {
+    max-width: 513px;
+  }
+`;
+
+
+export const TabWrapper1 = styled.div`
+  margin: 16px auto 0;
+  width: 100%;
+  padding: 40px 16px 40px 40px;
+  background: #F7F7F7;
+  border-radius: 0 32px 32px 0;
+
+  @media (min-width: 768px) {
+    padding: 40px;
+    margin: 0;
+  }
+  @media (min-width: 1024px) {
+    width: 500px;
+    height: 351px;
+  }
+  @media (min-width: 1280px) {
+    width: 628px;
+    height: 403px;
+    padding: 66px 40px 66px 124px;
+  }
+  @media (min-width: 1440px) {
+    width: 708px;
+    height: 380px;
+  }
+  @media (min-width: 1920px) {
+    width: 948px;
+    height: 391px;
+    padding: 66px 40px 66px 304px;
+  }
+`;
+
+export const TabWrapper3 = styled(TabWrapper1)`
+  padding: 40px 40px 79px 40px;
+  background: #363636;
+  border-radius: 32px;
+
+  @media (min-width: 768px) {
+    width: 376px;
+    height: 311px;
+    padding: 40px 40px 77px 40px;
+  }
+  @media (min-width: 1024px) {
+    width: 296px;
+    height: 342px;
+    padding: 40px 40px 50px 40px;
+  }
+  @media (min-width: 1280px) {
+    width: 328px;
+    height: 340px;
+    padding: 40px 40px 77px 40px;
+  }
+  @media (min-width: 1440px) {
+    width: 382px;
+    height: 298px;
+    padding: 40px 40px 56px 40px;
+  }
+  @media (min-width: 1920px) {
+    width: 421px;
+    height: 310px;
+  }
+`;
+
+export const TabWrapper5 = styled(TabWrapper1)`
+  padding: 40px;
+  border-radius: 32px 0 0 32px;
+
+  @media (min-width: 768px) {
+    width: 376px;
+    height: 311px;
+    margin: 16px 0 0;
+  }
+  @media (min-width: 1024px) {
+    width: 340px;
+    height: 342px;
+  }
+  @media (min-width: 1280px) {
+    width: 446px;
+    height: 340px;
+    padding: 40px 124px 40px 40px;
+  }
+  @media (min-width: 1440px) {
+    width: 552px;
+    height: 298px;
+  }
+  @media (min-width: 1920px) {
+    width: 726px;
+    height: 310px;
+    padding: 40px 304px 58px 40px;
+  }
+`;
+
+export const Year1 = styled.div`
+  font-family: 'Gilroy';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 25px;
+  color: #212121;
+
+  @media (min-width: 768px) {
+    font-size: 32px;
+    line-height: 38px;
+  }
+  @media (min-width: 1920px) {
+    font-size: 36px;
+    line-height: 42px;
+  }
+`;
+
+export const Year3 = styled(Year1)`
+  color: #FFF;
+
+  @media (min-width: 768px) {
+    font-size: 24px;
+    line-height: 29px;
+  }
+  @media (min-width: 1920px) {
+    font-size: 24px;
+    line-height: 29px;
+  }
+`;
+
+export const Year5 = styled(Year3)`
+  color: #212121;
+
+
+  @media (min-width: 768px) {
+
+  }
+  @media (min-width: 1920px) {
+
+  }
+`;
+
+export const List1 = styled.div`
+  padding: 16px 0 0;
+  margin: 0 auto;
+
+  @media (min-width: 768px) {
+    padding: 25px 0 0;
+  }
+`;
+
+export const List = styled(List1)`
+
+  @media (min-width: 768px) {
+    padding: 16px 0 0;
+  }
+  @media (min-width: 1280px) {
+    padding: 24px 0 0;
+  }
+`;
+
+export const LiWrapper1 = styled.div`
+  padding: 5px 0 0 15px;
+  margin: 12px 0 0;
+  position: relative;
+
+  &:first-child {
+    margin: 0;
+  }
+
+  @media (min-width: 768px) {
+    margin: 16px 0 0;
+  }
+`;
+
+export const LiWrapper = styled(LiWrapper1)`
+
+  @media (min-width: 768px) {
+    margin: 12px 0 0;
+  }
+  @media (min-width: 1280px) {
+
+  }
+`;
+
+export const Li1 = styled.div`
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 23px;
+  color: #5C5C5C;
+  z-index: 1;
+  position: relative;
+
+  @media (min-width: 768px) {
+    font-size: 16px;
+    line-height: 23px;
+  }
+  @media (min-width: 1920px) {
+    font-size: 18px;
+    line-height: 24px;
+  }
+`;
+
+export const Li = styled(Li1)`
+  color: #FFF;
+  line-height: 150%;
+
+  @media (min-width: 768px) {
+    font-size: 14px;
+  }
+  @media (min-width: 1920px) {
+    font-size: 16px;
+  }
+`;
+
+export const Li5 = styled(Li)`
+  color: #5C5C5C;
+
+  @media (min-width: 768px) {
+
+  }
+  @media (min-width: 1920px) {
+
+  }
+`;
+
+export const DotWrapper = styled.div`
+  width: 31px;
+  height: 31px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+export const LightDot = styled.div`
+  width: 22px;
+  height: 22px;
+  background: #FFF;
+  border-radius: 2px;
+  transform: rotate(-45deg);
+`;
+
+export const LightDot3 = styled(LightDot)`
+  background: radial-gradient(95.51% 95.51% at 50% 50%, rgba(109, 109, 109, 0.18) 0%, rgba(250, 250, 250, 0) 100%);
 `;
 
 
