6079a890be866445c794d6542a704f68c7f1fc2c
Kirill Usichenko <kirillusichenko@gmail.com>
2022-08-25 18:34:05 +0300

    fix: change tabs placement at dashboard component, feature page

--- src/components/pages/Feature/components/dashboard/Dashboard.tsx
+++ src/components/pages/Feature/components/dashboard/Dashboard.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
-import {H1, Img, Option, Options, P, Text, Title, Wrapper} from "./DashboardStyles";
+import {
+    Dot,
+    DotWrapper,
+    H1,
+    Img,
+    Option,
+    Options,
+    OptionsWrapper1,
+    OptionsWrapper2,
+    P,
+    Text,
+    Title,
+    Wrapper
+} from "./DashboardStyles";
 
 const Dashboard = () => {
     return (
@@ -8,28 +21,53 @@ const Dashboard = () => {
                 <H1>Supercharge your business with Tinvio</H1>
                 <Wrapper>
                     <Options>
-                        <Option>
-                            <Title>Chats</Title>
-                            <P>
-                                Send messages, invoices, and payment links directly to customers (even if they’re not on
-                                Tinvio)
-                            </P>
-                        </Option>
-                        <Option>
-                            <Title>Orders</Title>
-                            <P>
-                                Receive, manage, and track all your orders in a format designed to prevent mistakes
-                            </P>
-                        </Option>
-                        <Option>
-                            <Title>Payments</Title>
-                            <P>
-                                Collect payments and reconcile them against invoices, without checking bank statements
-                            </P>
-                        </Option>
-                        <Text>
-                            And more in one dashboard...
-                        </Text>
+                        <OptionsWrapper1>
+                            <Option>
+                                <Title>
+                                    <DotWrapper>
+                                        <Dot/>
+                                    </DotWrapper>
+                                    Chats
+                                </Title>
+                                <P>
+                                    Send messages, invoices, and payment links directly to customers (even if they’re
+                                    not on
+                                    Tinvio)
+                                </P>
+                            </Option>
+                            <Option>
+                                <Title>
+                                    <DotWrapper>
+                                        <Dot/>
+                                    </DotWrapper>
+                                    Orders
+                                </Title>
+                                <P>
+                                    Receive, manage, and track all your orders in a format designed to prevent mistakes
+                                </P>
+                            </Option>
+                        </OptionsWrapper1>
+
+                        <OptionsWrapper2>
+                            <Option>
+                                <Title>
+                                    <DotWrapper>
+                                        <Dot/>
+                                    </DotWrapper>
+                                    Payments
+                                </Title>
+                                <P>
+                                    Collect payments and reconcile them against invoices, without checking bank
+                                    statements
+                                </P>
+                            </Option>
+                            <Text>
+                                <DotWrapper>
+                                    <Dot/>
+                                </DotWrapper>
+                                And more in one dashboard...
+                            </Text>
+                        </OptionsWrapper2>
                     </Options>
                     <Img src='/assets/images/features/supercharge-screen.png'/>
                 </Wrapper>
--- src/components/pages/Feature/components/dashboard/DashboardStyles.tsx
+++ src/components/pages/Feature/components/dashboard/DashboardStyles.tsx
@@ -30,7 +30,6 @@ export const H1 = styled.div`
 `;
 
 export const Wrapper = styled.div`
-  width: 100%;
   border-radius: 0 50px 50px 0;
   background: #363636;
   margin: 24px 0 0;
@@ -39,24 +38,74 @@ export const Wrapper = styled.div`
   @media (min-width: 768px) {
     margin: 32px 0 0;
   }
+  @media (min-width: 1024px) {
+    display: flex;
+    padding: 12px 0;
+  }
+  @media (min-width: 1280px) {
+    margin: 32px 54px 0 0;
+  }
+  @media (min-width: 1920px) {
+    margin: 32px 234px 0 0;
+  }
 `;
 
 export const Options = styled.div`
   padding: 47px 36px 0 36px;
   
   @media (min-width: 768px) {
-    padding: 80px 48px 0 48px;
-    display: flex;
-    flex-wrap: wrap;
+    padding: 80px 118px 0 48px;
+    max-width: 768px;
   }
   @media (min-width: 1024px) {
-    padding: 96px 40px 84px 40px;
+    padding: 80px 68px 72px 40px;
+    max-width: unset;
+    width: 553px;
   }
   @media (min-width: 1280px) {
-    padding: 96px 124px 84px 124px;
+    padding: 80px 83px 84px 124px;
+    width: 652px;
+  }
+  @media (min-width: 1440px) {
+    padding: 80px 144px 84px 124px;
+    width: 732px;
   }
   @media (min-width: 1920px) {
-    padding: 87px 306px 75px 306px;
+    padding: 87px 178px 75px 306px;
+    width: 1035px;
+  }
+`;
+
+export const OptionsWrapper1 = styled.div`
+  
+  @media (min-width: 768px) {
+    display: flex;
+    justify-content: space-between;
+  }
+  @media (min-width: 1024px) {
+  }
+  @media (min-width: 1280px) {
+  }
+  @media (min-width: 1920px) {
+  }
+`;
+
+export const OptionsWrapper2 = styled.div`
+  padding: 43px 0 0;
+  
+  @media (min-width: 768px) {
+    padding: 108px 0 0;
+    width: fit-content;
+  }
+  @media (min-width: 1024px) {
+    width: unset;
+    display: flex;
+    justify-content: space-between;
+    padding: 56px 0 0;
+  }
+  @media (min-width: 1280px) {
+  }
+  @media (min-width: 1920px) {
   }
 `;
 
@@ -68,6 +117,7 @@ export const Option = styled.div`
   
   @media (min-width: 768px) {
     max-width: 236px;
+    margin: 0;
   }
   @media (min-width: 1024px) {
     
@@ -76,11 +126,11 @@ export const Option = styled.div`
     
   }
   @media (min-width: 1920px) {
-    
   }
 `;
 
 export const Title = styled.div`
+  position: relative;
   font-family: 'Gilroy';
   font-style: normal;
   font-weight: 700;
@@ -90,6 +140,37 @@ export const Title = styled.div`
   
   @media (min-width: 1920px) {
     font-size: 28px;
+  }
+`;
+
+export const DotWrapper = styled.div`
+  width: 49px;
+  height: 49px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: -7px;
+  left: -20px;
+
+  @media (min-width: 768px) {
+    width: 63px;
+    height: 63px;
+    top: -13px;
+    left: -31px;
+  }
+`;
+
+export const Dot = styled.div`
+  width: 34px;
+  height: 34px;
+  background: rgba(109, 109, 109, 0.18);
+  border-radius: 4px;
+  transform: rotate(-45deg);
+
+  @media (min-width: 768px) {
+    width: 44px;
+    height: 44px;
   }
 `;
 
@@ -102,14 +183,16 @@ export const P = styled.p`
   color: #FAFAFA;
   
   @media (min-width: 1024px) {
-    
+    max-width: 191px;
   }
   @media (min-width: 1920px) {
     font-size: 16px;
+    max-width: 237px;
   }
 `;
 
 export const Text = styled.div`
+  position: relative;
   font-family: 'Gilroy';
   font-style: normal;
   font-weight: 700;
@@ -118,23 +201,40 @@ export const Text = styled.div`
   color: #FFFFFF;
   height: fit-content;
 
+  @media (min-width: 768px) {
+    margin: 108px 0 0;
+    max-width: 198px;
+  }
+  @media (min-width: 1024px) {
+    margin: 0 -6px 0 0
+  }
   @media (min-width: 1920px) {
     font-size: 28px;
+    max-width: 235px;
+    margin: 0;
   }
 `;
 
 export const Img = styled.img`
-  max-width: 462px;
+  min-width: 462px;
   padding: 40px 0 0;
-  margin: 0 -139px 0 0px;
+  margin: 0 -103px 0 0px;
   float: right;
 
   @media (min-width: 768px) {
     max-width: 690px;
     padding: 0;
-    margin: 0 -500px 0 0;
+    margin: -347px -245px -30px 0;
   }
   @media (min-width: 1024px) {
-    
+    max-height: 460px;
+    min-width: 690px;
+    margin: 0 -219px 0 0;
+  }
+  @media (min-width: 1280px) {
+    margin: 0 -240px 0 0;
+  }
+  @media (min-width: 1440px) {
+    margin: 0 -36px 0 0;
   }
 `;
